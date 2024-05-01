bbc9da93a745581bebc70ffb8a5f0f9f52731141
Miči Hrešková <hroncovam@yahoo.com>
2024-05-01 20:27:34 +0200

    jQuery 2. video

--- 03 JS/01 uvod/js/script.js
+++ 03 JS/01 uvod/js/script.js
@@ -1,3 +1,22 @@
+//VIDEO 2
+$('.post-title').css({
+    background: 'blue',
+    color: 'yellow'
+});
+
+let obj = {
+    background: 'yellow',
+    color: 'blue'
+}
+$('#wow-element').css(obj);
+
+console.log($('#wow-element').css('background'));
+
+$('.slow-hide').text(); //getter
+$('.slow-hide').text('wooooooooooooooooow'); //setter
+
+//VIDEO 1
+/*
 $('.post-title').hide();
 $('.post-title').show();
 $('h2').hide();
@@ -9,3 +28,4 @@ $('.fade-out').fadeOut(5000);
 $('.fade-out').fadeIn(5000);
 
 $('.post-title').hide().fadeIn(); //method chaining
+*/
