534cf76f3daac5206e1b62d5fd03e15b785518b9
Miči Hrešková <hroncovam@yahoo.com>
2024-05-28 21:19:31 +0100

    pohyb aj hore dole

--- 04 modern JS/01/js/app.js
+++ 04 modern JS/01/js/app.js
@@ -9,16 +9,25 @@ addMessage("browser", "page loaded");
 document.body.addEventListener("click", function clickedIt() { addMessage("user", "clicked") });
 
 document.body.addEventListener("keyup", function moveIt(event) { 
-    let current = getComputedStyle(document.body).left;
-    let newX;
-    current = parseInt(current);
+    let currentX = getComputedStyle(document.body).left,
+        currentY = getComputedStyle(document.body).top;
+    let newX, newY;
+
+    currentX = parseInt(currentX);
+    currentY = parseInt(currentY);
+    
     if (event.key == "ArrowLeft") {
-        newX = current - 10;
+        newX = currentX - 10;
     } else if (event.key == "ArrowRight") {
-        newX = current + 10;
+        newX = currentX + 10;
+    } else if (event.key == "ArrowUp") {
+        newY = currentY - 10;
+    } else if (event.key == "ArrowDown") {
+        newY = currentY + 10;
     } else {
         addMessage("user", "key pressed " + event.key) 
     } 
     
     document.body.style.left = newX + "px";
+    document.body.style.top = newY + "px";
 });
